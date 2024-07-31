678b56d5470e2e7b33dc1eb1a192fae521a9b645
Eth-Ski <105698499+Eth-Ski@users.noreply.github.com>
2024-07-31 12:37:53 +1000

    Update postpage.js

--- my-pregnancy/src/pages/post/postpage.js
+++ my-pregnancy/src/pages/post/postpage.js
@@ -1,12 +1,14 @@
 import Footer from "../../global-components/footer/footer";
 import Navbar from "../../global-components/navbar2/navbar2.js"
 
+import PostHome from "./posthome.js";
+
 function PostPage(){
   return (
     <div>
       <div style={{minHeight: 'calc(100vh - 202px)'}}>
         <Navbar></Navbar>
-        <h1>post</h1>
+        <PostHome></PostHome>
       </div>
       
       <Footer></Footer>
