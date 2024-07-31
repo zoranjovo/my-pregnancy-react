b4a39b0c23790eab8b63facdcc368aed6e9820fa
Eth-Ski <105698499+Eth-Ski@users.noreply.github.com>
2024-07-31 13:16:07 +1000

    Update fitnesshome.js

--- my-pregnancy/src/pages/fitness/fitnesshome.js
+++ my-pregnancy/src/pages/fitness/fitnesshome.js
@@ -32,7 +32,7 @@ function FitnessHome(){
   
   return (
     <div className={styles.outerdiv}>
-      <h1 className="text-3xl font-medium text-blue">Fitness Videos and Webinars</h1>
+      <h1 className="text-3xl font-bold text-blue">Fitness Videos and Webinars</h1>
       <div className={styles.innerdiv}>
         <input 
           className={`${styles.searchbox} block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
